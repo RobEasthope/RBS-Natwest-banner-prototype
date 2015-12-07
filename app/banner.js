92fbae8ebcc88fe84cefb0277db618fd1f171fb3
Rob Easthope <robeasthope@gmail.com>
2015-12-07 15:42:22 +0000

    Animations reversion to get around issues

--- app/banner.js
+++ app/banner.js
@@ -169,9 +169,12 @@ function animationSequence(){
   .from('#F2', frameAnimationTiming, {css: { top: entranceDistance, left: 0, opacity: 0 }})
   .to('#F2', frameAnimationTiming, {css: { top: exitDistance, opacity: 0 }}, holdTime)
 
+  .to('#logo', frameAnimationTiming, {css: { opacity: 0 }})
+
   // Frame #3
   .from('#F3', frameAnimationTiming, {css: { top: entranceDistance, left: 0, opacity: 0 }})
   .to('#F3', frameAnimationTiming, {css: { top: exitDistance, opacity: 0 }}, holdTime)
+
 
   // Frame #4
   .from('#F4', frameAnimationTiming, {css: { top: entranceDistance, left: 0, opacity: 0 }}, 'F4-L1-entrance')
@@ -181,7 +184,7 @@ function animationSequence(){
   .to('body', 0, {css: { top: 0 }}, finalHoldTime)
 
   // Exit Frame4
-  .to('#F4', frameAnimationTiming, {css: { top: exitDistance, opacity: 0 }}, 'F4-L1-exit')
+  .to('#F4', frameAnimationTiming, {css: { top: exitDistance, opacity: 0 }}, 'F4-L2-exit')
   .to('#logo', frameAnimationTiming, {css: { opacity: 0 }}, 'F4-L2-exit')
 
   // Frame #5
@@ -205,7 +208,8 @@ function animationSequence(){
 
   // LOOP TWO
   // Slide in background
-  .to('#bkg', bkgAnimationTiming, {css: { left: 0 }})
+  .to('#bkg', bkgAnimationTiming, {css: { left: 0 }}, 'bkg-loop-2')
+  .to('#logo', frameAnimationTiming, {css: { opacity: 1 }}, 'bkg-loop-2')
 
   // Frame #1
   .fromTo('#F1', frameAnimationTiming, {css: { top: entranceDistance, left: 0, opacity: 0 }}, {css: { top: 0, left: 0, opacity: 1 }})
@@ -214,6 +218,8 @@ function animationSequence(){
   // Frame #2
   .fromTo('#F2', frameAnimationTiming, {css: { top: entranceDistance, left: 0, opacity: 0 }}, {css: { top: 0, left: 0, opacity: 1 }})
   .to('#F2', frameAnimationTiming, {css: { top: exitDistance, opacity: 0 }}, holdTime)
+
+  .to('#logo', frameAnimationTiming, {css: { opacity: 0 }})
 
   // Frame #3
   .fromTo('#F3', frameAnimationTiming, {css: { top: entranceDistance, left: 0, opacity: 0 }}, {css: { top: 0, left: 0, opacity: 1 }})
